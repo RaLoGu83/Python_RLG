4d5ab1b70a5830b04be31473d6103095d8757c89
Ralo <ralogu83@gmail.com>
2025-11-05 14:21:21 +0100

    ej15

---
+++ UD3/Ej15.py
@@ -0,0 +1,9 @@
+Num1 = int(input("Introduce el primer número: "))
+Num2 = int(input("Introduce el segundo número: "))
+
+if Num1 > Num2:
+    print(f"El número mayor es: {Num1}")
+elif Num2 > Num1:
+    print(f"El número mayor es: {Num2}")
+else:
+    print("Ambos números son iguales.")
