d58ecb2fcc7c2facb3feb4ffec75c0f9eff12cad
Ralo <ralogu83@gmail.com>
2025-11-05 14:18:34 +0100

    ej14

---
+++ UD3/Ej14.py
@@ -0,0 +1,7 @@
+Num1 = int(input("Introduce el primer número: "))
+Num2 = int(input("Introduce el segundo número: "))
+
+print(f"La suma es: {Num1 + Num2}")
+print(f"La resta es: {Num1 - Num2}")
+print(f"La multiplicación es: {Num1 * Num2}")
+print(f"La división es: {Num1 / Num2}")
