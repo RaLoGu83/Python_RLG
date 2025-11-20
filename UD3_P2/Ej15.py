031fa38a30e1a43a90b137c9d2fe343dbb004313
Ralo <ralogu83@gmail.com>
2025-11-20 17:00:12 +0100

    ej15

---
+++ UD3_P2/Ej15.py
@@ -0,0 +1,7 @@
+altura = int(input("Introduce la altura de la pirámide: "))
+ast = "*"
+espacios = " "
+
+for i in range (1, altura):
+    print(espacios*i + ((altura-i)*ast)+ast + ((altura-i)*ast))
+print(altura* espacios + ast)
