aed2c40c367e1218d77f4377e01b02a670923091
Ralo <ralogu83@gmail.com>
2025-11-24 09:43:39 +0100

    foro

---
+++ Foro/manipulación básica de texto sin listas.py
@@ -0,0 +1,27 @@
+# Leer una cadena desde teclado y mostrarla carácter por carácter usando un ciclo for y el índice.
+
+# cadena = str(input("Introduce una cadena de texto: "))
+
+# for i in range(len(cadena)):
+#     print(cadena[i])
+
+
+# Concatenar caracteres o cadenas utilizando el operador + para formar nuevas cadenas.
+
+# cadena = str(input("Introduce una cadena de texto: "))
+# cadena2 = str(input("Introduce otra cadena de texto: "))
+
+# print(cadena + " " + cadena2)
+
+
+# Contar cuántas veces aparece un carácter dado en una cadena usando for y un contador.
+
+# cadena = str(input("Mete la cadena: ")) 
+# contador = 0
+# for i in range(len(cadena)):
+#     contador += 1
+# print(contador)
+
+
+# Construir manualmente una nueva cadena añadiendo un carácter a la vez (ejemplo: filtrar caracteres o construir cadenas invertidas).
+
