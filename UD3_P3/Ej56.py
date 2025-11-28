a481ca08fe71c708fded99564c66e4ffed876a0e
Ralo <ralogu83@gmail.com>
2025-11-28 09:14:42 +0100

    no entiendo este código

---
+++ UD3_P3/Ej56.py
@@ -0,0 +1,19 @@
+n = int(input("Ingrese el número de primos: "))
+num = 2
+cont = 0
+
+while cont < n:
+    es_primo = True
+    i = 2
+    
+    while i * i <= num:
+        if num % i == 0:
+            es_primo = False
+            break
+        i += 1
+
+    if es_primo:
+        print(num, end=" ")
+        cont += 1
+
+    num += 1
