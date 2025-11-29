b94b0c0239a7a9591a5cf7d2c82ca5b17402c3ab
Ralo <ralogu83@gmail.com>
2025-11-29 10:19:20 +0100

    ej23

---
+++ UD3_P3/Ej23.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+if num2 != 0:
+    print("La división es:", num1 / num2)
+else:
+    print("No se puede dividir entre cero.")
