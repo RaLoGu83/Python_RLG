c69acad64b4ca01895e5539dacbecdea198b162c
Ralo <ralogu83@gmail.com>
2025-11-29 10:17:49 +0100

    ej20

---
+++ UD3_P3/Ej20.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+if num1 > num2:
+    print("El primer número es mayor que el segundo.")
+else:
+    print("El primer número NO es mayor que el segundo.")
