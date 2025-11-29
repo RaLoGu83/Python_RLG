01903872ba4015b8fc91bb8f4415a571e77a097a
Ralo <ralogu83@gmail.com>
2025-11-29 10:20:04 +0100

    ej24

---
+++ UD3_P3/Ej24.py
@@ -0,0 +1,7 @@
+usuario = input("Ingrese el nombre de usuario: ")
+contra = input("Ingrese la contraseña: ")
+
+if usuario == "pepe" and contra == "asdasd":
+    print("Has entrado al sistema")
+else:
+    print("Error: usuario o contraseña incorrectos")
