18da588ab4458bc538f0574774995f08fcaea81b
Ralo <ralogu83@gmail.com>
2025-11-29 10:10:21 +0100

    ej18

---
+++ UD3_P3/Ej18.py
@@ -0,0 +1,9 @@
+nombre = str(input("Nombre: "))
+ape1 = str(input("Apellido1: "))
+ape2 = str(input("Apellido2: "))
+
+nombre = nombre.upper()
+ape1 = ape1.upper()
+ape2 = ape2.upper()
+
+print(nombre[0], ape1[0], ape2[0])
