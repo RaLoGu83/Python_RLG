04b785cf8b980efd514ae36f62f32853d930b837
Ralo <ralogu83@gmail.com>
2025-12-01 08:43:49 +0100

    ej35

---
+++ UD3_P3/Ej35.py
@@ -0,0 +1,18 @@
+n = int(input("Día de la semana (1-7): "))
+
+if n == 1:
+    print("Lunes")
+elif n == 2:
+    print("Martes")
+elif n == 3:
+    print("Miércoles")
+elif n == 4:
+    print("Jueves")
+elif n == 5:
+    print("Viernes")
+elif n == 6:
+    print("Sábado")
+elif n == 7:
+    print("Domingo")
+else:
+    print("Error")
